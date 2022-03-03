532fe3115cd5cd8b088a93279b8c52a46b02a73d
Asami119 <iamas119@yahoo.co.jp>
2022-03-03 11:10:15 +0900

    Postモデルの単体テストを実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :post do
-    
+    date_of_post { Faker::Date.between(from: 31.days.ago, to: Date.today) }
+    price        { Faker::Number.between(from: 1, to: 100000) }
+    memo_1       { Faker::Lorem.word }
+    memo_2       { Faker::Lorem.word }
+    association :user
   end
 end
