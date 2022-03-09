a568c5c2102da27a4ecb1686d22eed0ac540575f
Asami119 <iamas119@yahoo.co.jp>
2022-03-09 17:14:18 +0900

    rubocopの指摘箇所を修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,10 +40,10 @@ class PostsController < ApplicationController
     set_memo_search
     @q = current_user.posts.ransack(params[:q])
     @posts = @q.result.order(date_of_post: 'DESC')
-    @price_for_graph, @price_true_percent = Post.calc_for_donut_graph(@posts)
-    @price_month, @price_year = Post.calc_for_column_graph(@posts)
+    @price_for_graph, @price_true_percent = Post.calc_donut(@posts)
+    @price_month, @price_year = Post.calc_column(@posts)
 
-    @price_month_average = Post.calc_price_month_average(@price_month, @price_year) unless @price_year == 0
+    @price_month_average = Post.calc_month_average(@price_month, @price_year) unless @price_year.zero?
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ApplicationRecord
     ).where(date_of_post: Date.today.beginning_of_month..Date.today.end_of_month).order(date_of_post: 'DESC')
   end
 
-  def self.calc_for_donut_graph(posts)
+  def self.calc_donut(posts)
     price_true = posts.where(select_yen: true).sum(:price)
     price_false = posts.where(select_yen: false).sum(:price)
     total_price = (price_true + price_false).to_f
@@ -19,88 +19,90 @@ class Post < ApplicationRecord
     [{ 'good yen!': price_true, 'あと一歩': price_false }, price_true_percent]
   end
 
-  def self.calc_for_column_graph(posts)
+  def self.calc_column(posts)
     year = Date.today.year
     data_year = posts.where(date_of_post: "#{year}-01-01".."#{year}-12-31")
     price_year = data_year.sum(:price)
-    data_year_true = data_year.where(select_yen: true)
-    data_year_falce = data_year.where(select_yen: false)
 
     # 1月
-    data_Jan = data_year.where(date_of_post: "#{year}-01-01".."#{year}-01-31")
-    price_Jan_true = data_Jan.where(select_yen: true).sum(:price)
-    price_Jan_false = data_Jan.where(select_yen: false).sum(:price)
+    month1 = data_year.where(date_of_post: "#{year}-01-01".."#{year}-01-31")
+    price_month1_true = month1.where(select_yen: true).sum(:price)
+    price_month1_false = month1.where(select_yen: false).sum(:price)
 
     # 2月
-    first_Feb = Date.new(year, 2, 1)
-    data_Feb = data_year.where(date_of_post: first_Feb..first_Feb.end_of_month)
-    price_Feb_true = data_Feb.where(select_yen: true).sum(:price)
-    price_Feb_false = data_Feb.where(select_yen: false).sum(:price)
+    first_day_month2 = Date.new(year, 2, 1)
+    month2 = data_year.where(date_of_post: first_day_month2..first_day_month2.end_of_month)
+    price_month2_true = month2.where(select_yen: true).sum(:price)
+    price_month2_false = month2.where(select_yen: false).sum(:price)
 
     # 3月
-    data_Mar = data_year.where(date_of_post: "#{year}-03-01".."#{year}-03-31")
-    price_Mar_true = data_Mar.where(select_yen: true).sum(:price)
-    price_Mar_false = data_Mar.where(select_yen: false).sum(:price)
+    month3 = data_year.where(date_of_post: "#{year}-03-01".."#{year}-03-31")
+    price_month3_true = month3.where(select_yen: true).sum(:price)
+    price_month3_false = month3.where(select_yen: false).sum(:price)
 
     # 4月
-    data_Apr = data_year.where(date_of_post: "#{year}-04-01".."#{year}-04-30")
-    price_Apr_true = data_Apr.where(select_yen: true).sum(:price)
-    price_Apr_false = data_Apr.where(select_yen: false).sum(:price)
+    month4 = data_year.where(date_of_post: "#{year}-04-01".."#{year}-04-30")
+    price_month4_true = month4.where(select_yen: true).sum(:price)
+    price_month4_false = month4.where(select_yen: false).sum(:price)
 
     # 5月
-    data_May = data_year.where(date_of_post: "#{year}-05-01".."#{year}-05-31")
-    price_May_true = data_May.where(select_yen: true).sum(:price)
-    price_May_false = data_May.where(select_yen: false).sum(:price)
+    month5 = data_year.where(date_of_post: "#{year}-05-01".."#{year}-05-31")
+    price_month5_true = month5.where(select_yen: true).sum(:price)
+    price_month5_false = month5.where(select_yen: false).sum(:price)
 
     # 6月
-    data_Jun = data_year.where(date_of_post: "#{year}-06-01".."#{year}-06-30")
-    price_Jun_true = data_Jun.where(select_yen: true).sum(:price)
-    price_Jun_false = data_Jun.where(select_yen: false).sum(:price)
+    month6 = data_year.where(date_of_post: "#{year}-06-01".."#{year}-06-30")
+    price_month6_true = month6.where(select_yen: true).sum(:price)
+    price_month6_false = month6.where(select_yen: false).sum(:price)
 
     # 7月
-    data_Jul = data_year.where(date_of_post: "#{year}-07-01".."#{year}-07-31")
-    price_Jul_true = data_Jul.where(select_yen: true).sum(:price)
-    price_Jul_false = data_Jul.where(select_yen: false).sum(:price)
+    month7 = data_year.where(date_of_post: "#{year}-07-01".."#{year}-07-31")
+    price_month7_true = month7.where(select_yen: true).sum(:price)
+    price_month7_false = month7.where(select_yen: false).sum(:price)
 
     # 8月
-    data_Aug = data_year.where(date_of_post: "#{year}-08-01".."#{year}-08-31")
-    price_Aug_true = data_Aug.where(select_yen: true).sum(:price)
-    price_Aug_false = data_Aug.where(select_yen: false).sum(:price)
+    month8 = data_year.where(date_of_post: "#{year}-08-01".."#{year}-08-31")
+    price_month8_true = month8.where(select_yen: true).sum(:price)
+    price_month8_false = month8.where(select_yen: false).sum(:price)
 
     # 9月
-    data_Sep = data_year.where(date_of_post: "#{year}-09-01".."#{year}-09-30")
-    price_Sep_true = data_Sep.where(select_yen: true).sum(:price)
-    price_Sep_false = data_Sep.where(select_yen: false).sum(:price)
+    month9 = data_year.where(date_of_post: "#{year}-09-01".."#{year}-09-30")
+    price_month9_true = month9.where(select_yen: true).sum(:price)
+    price_month9_false = month9.where(select_yen: false).sum(:price)
 
     # 10月
-    data_Oct = data_year.where(date_of_post: "#{year}-10-01".."#{year}-10-31")
-    price_Oct_true = data_Oct.where(select_yen: true).sum(:price)
-    price_Oct_false = data_Oct.where(select_yen: false).sum(:price)
+    month10 = data_year.where(date_of_post: "#{year}-10-01".."#{year}-10-31")
+    price_month10_true = month10.where(select_yen: true).sum(:price)
+    price_month10_false = month10.where(select_yen: false).sum(:price)
 
     # 11月
-    data_Nov = data_year.where(date_of_post: "#{year}-11-01".."#{year}-11-30")
-    price_Nov_true = data_Nov.where(select_yen: true).sum(:price)
-    price_Nov_false = data_Nov.where(select_yen: false).sum(:price)
+    month11 = data_year.where(date_of_post: "#{year}-11-01".."#{year}-11-30")
+    price_month11_true = month11.where(select_yen: true).sum(:price)
+    price_month11_false = month11.where(select_yen: false).sum(:price)
 
     # 12月
-    data_Dec = data_year.where(date_of_post: "#{year}-12-01".."#{year}-12-31")
-    price_Dec_true = data_Dec.where(select_yen: true).sum(:price)
-    price_Dec_false = data_Dec.where(select_yen: false).sum(:price)
+    month12 = data_year.where(date_of_post: "#{year}-12-01".."#{year}-12-31")
+    price_month12_true = month12.where(select_yen: true).sum(:price)
+    price_month12_false = month12.where(select_yen: false).sum(:price)
 
     [
       [
         { name: 'Good yen!',
-          data: [['1月', price_Jan_true], ['2月', price_Feb_true], ['3月', price_Mar_true], ['4月', price_Apr_true], ['5月', price_May_true],
-                 ['6月', price_Jun_true], ['7月', price_Jul_true], ['8月', price_Aug_true], ['9月', price_Sep_true], ['10月', price_Oct_true], ['11月', price_Nov_true], ['12月', price_Dec_true]] },
+          data: [['1月', price_month1_true], ['2月', price_month2_true], ['3月', price_month3_true],
+                 ['4月', price_month4_true], ['5月', price_month5_true], ['6月', price_month6_true],
+                 ['7月', price_month7_true], ['8月', price_month8_true], ['9月', price_month9_true],
+                 ['10月', price_month10_true], ['11月', price_month11_true], ['12月', price_month12_true]] },
         { name: 'あと一歩',
-          data: [['1月', price_Jan_false], ['2月', price_Feb_false], ['3月', price_Mar_false], ['4月', price_Apr_false],
-                 ['5月', price_May_false], ['6月', price_Jun_false], ['7月', price_Jul_false], ['8月', price_Aug_false], ['9月', price_Sep_false], ['10月', price_Oct_false], ['11月', price_Nov_false], ['12月', price_Dec_false]] }
+          data: [['1月', price_month1_false], ['2月', price_month2_false], ['3月', price_month3_false],
+                 ['4月', price_month4_false], ['5月', price_month5_false], ['6月', price_month6_false],
+                 ['7月', price_month7_false], ['8月', price_month8_false], ['9月', price_month9_false],
+                 ['10月', price_month10_false], ['11月', price_month11_false], ['12月', price_month12_false]] }
       ],
       price_year
     ]
   end
 
-  def self.calc_price_month_average(price_month, price_year)
+  def self.calc_month_average(price_month, price_year)
     array_true = []
     array_false = []
     count_month = 0
@@ -114,7 +116,7 @@ class Post < ApplicationRecord
     end
 
     array_true.zip(array_false) do |t, f|
-      count_month += 1 unless t == 0 && f == 0
+      count_month += 1 unless t.zero? && f.zero?
     end
 
     (price_year / count_month)
