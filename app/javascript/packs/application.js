25e132174390ce88f89a865a5b190af1a963fc51
Asami119 <iamas119@yahoo.co.jp>
2022-03-24 15:27:18 +0900

    検索フォームのリセットボタンを作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 import 'chartkick/highcharts'
+require("../search")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
