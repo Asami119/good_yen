75f891573b79945a307d34fe1b2a3a5d5fa087b4
Asami119 <iamas119@yahoo.co.jp>
2022-03-24 16:39:28 +0900

    require countを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 require("jquery")
 import 'chartkick/highcharts'
 require("../reset")
-require("../count")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
