70af8520c7a5a4c7a2e8e49c34aedfbff9b93054
Asami119 <iamas119@yahoo.co.jp>
2022-04-02 19:44:55 +0900

    Good yen 選択時のイベントを最適化

--- app/javascript/good_yen.js
+++ app/javascript/good_yen.js
@@ -1,21 +1,19 @@
 function good_yen (){
-  const goodYenLabel = document.getElementById('good_yen_label');
-  const notGoodYenLabel = document.getElementById('not_good_yen_label');
-
-  goodYenLabel.addEventListener('click', () => {
-    const insertPoint = document.getElementById('insert_point');
-    const messages = ['Good yen!', 'Good yen!', 'Good yen!', 'ありがとう！', 'いいね！', 'ステキ！', 'さすがです！', '最高！'];
-    const messageNo = Math.floor(Math.random() * messages.length);
-    const html = `
-      <div id="id_balloon">
-        <p class="balloon">${messages[messageNo]}</p>
-      </div>`;
-    insertPoint.insertAdjacentHTML('afterend', html);
-  });
-
-  notGoodYenLabel.addEventListener('click', () => {
+  const radioButton = document.getElementById('radio_button')
+  radioButton.addEventListener('change', () => {
     const html = document.getElementById('id_balloon')
-    html.remove();
+    if (html == null) {
+      const insertPoint = document.getElementById('insert_point');
+      const messages = ['Good yen!', 'Good yen!', 'Good yen!', 'ありがとう！', 'いいね！', 'ステキ！', 'さすがです！', '最高！'];
+      const messageNo = Math.floor(Math.random() * messages.length);
+      const html = `
+        <div id="id_balloon">
+          <p class="balloon">${messages[messageNo]}</p>
+        </div>`;
+      insertPoint.insertAdjacentHTML('afterend', html);
+    } else {
+        html.remove();    
+      };
   });
 };
 
