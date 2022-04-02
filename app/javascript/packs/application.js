76c920221de29551d1c44bfb57da3829a7ca7bf7
Asami119 <iamas119@yahoo.co.jp>
2022-04-02 11:30:41 +0900

    金額の表示・非表示を実装（ドーナツグラフ）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("jquery")
 import 'chartkick/highcharts'
 require("../reset")
+require("../show_hide")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
