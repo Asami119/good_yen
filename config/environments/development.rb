989c741877e9a6eb72aaf94c82ac15a3edf33b11
Asami119 <iamas119@yahoo.co.jp>
2022-04-05 17:11:53 +0900

    パスワード再設定を本番環境でも行えるようにする

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,8 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'good-yen.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
