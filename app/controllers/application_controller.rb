a24964e49879dc5ce84ad92b23cefa09f6fa173a
Asami119 <iamas119@yahoo.co.jp>
2022-04-05 12:08:41 +0900

    Rubocopを実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,11 +20,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     new_post_path
   end
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     new_post_path
   end
 end
